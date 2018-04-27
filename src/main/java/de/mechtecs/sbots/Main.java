88946117264f7fe2814b8cd28c8b232533df2018
Maximilian Schorradt <schorradt@publicplan.de>
2018-04-27 14:00:36 +0200

    Lade Welten per CLI

--- src/main/java/de/mechtecs/sbots/Main.java
+++ src/main/java/de/mechtecs/sbots/Main.java
@@ -1,6 +1,8 @@
 package de.mechtecs.sbots;
 
 import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 
 public class Main {
 
@@ -8,15 +10,39 @@ public class Main {
         World w = new World();
         View view;
 
-        int viewOption = JOptionPane.showOptionDialog(null, "Which View do you want to start?", null, JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[]{"DumbView", "LWJGLView"}, null);
-        switch (viewOption) {
-            case 0:
-                view = new DumbView();
-                break;
-            case 1:
-            default:
-                view = new LWJGLView();
-                break;
+        if (args.length < 1 || args.length > 2) {
+            int viewOption = JOptionPane.showOptionDialog(null, "Which View do you want to start?", null, JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, new String[]{"DumbView", "LWJGLView"}, null);
+            switch (viewOption) {
+                case 0:
+                    view = new DumbView();
+                    break;
+                case 1:
+                default:
+                    view = new LWJGLView();
+                    break;
+            }
+        } else {
+            switch (args[0]) {
+                case "dumbview":
+                default:
+                    view = new DumbView();
+                    break;
+                case "lwjglview":
+                    view = new LWJGLView();
+                    break;
+            }
+
+            if (args.length == 2) {
+                try {
+                    FileInputStream fis = new FileInputStream(args[1]);
+                    ObjectInputStream ois = new ObjectInputStream(fis);
+                    w = (World) ois.readObject();
+                    ois.close();
+                    fis.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         view.setWorld(w);
